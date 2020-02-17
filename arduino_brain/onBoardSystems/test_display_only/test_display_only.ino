eb8350b1c0879bc4de742a1a0216c91f376dc636
Quentin Van Overmeere <49268972+vquentin@users.noreply.github.com>
2020-02-17 14:03:51 +0100

    Included distances trip, odo, battery remaining

--- arduino_brain/onBoardSystems/test_display_only/test_display_only.ino
+++ arduino_brain/onBoardSystems/test_display_only/test_display_only.ino
@@ -32,13 +32,19 @@ All text above, and the splash screen below must be included in any redistributi
 // battery-drawing parameters
 #define SCREEN_BATTERY_IND_HEIGHT 41
 #define SCREEN_BATTERY_IND_WIDTH 20
-#define SCREEN_BATTERY_IND_POS_X 106
-#define SCREEN_BATTERY_IND_POS_Y 9
+#define SCREEN_BATTERY_IND_POS_X 100
+#define SCREEN_BATTERY_IND_POS_Y 8
 #define SCREEN_BATTERY_IND_CAP_HEIGHT 5
 #define SCREEN_BATTERY_IND_CAP_WIDTH 10
 #define SCREEN_BATTERY_IND_N 5 // number of rectangles to draw
 #define SCREEN_BATTERY_IND_N_SPACE 2 // margin around the battery capacity indicators
 #define SCREEN_BATTERY_IND_N_HEIGHT 6 // height of rectangle for capacity indication. Could be computed but worse for rounding errors
+
+#define SCREEN_SPEED_X 20
+#define SCREEN_SPEED_Y 6
+
+#define SCREEN_ODO_X 10
+#define SCREEN_ODO_Y 55
 
 // software SPI
 Adafruit_SSD1305 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
@@ -54,22 +60,24 @@ void setup()   {
   // init done
   
   display.display(); // show splashscreen
-  delay(100);
+  delay(150);
   display.clearDisplay();   // clears the screen and buffer
   display.display();
 
-// Battery and speed indicator
+}
+
+void loop() {
+  // Battery and speed indicator
   for(uint8_t i = 0;i<=45;i++){
     display.clearDisplay();
     drawBattery(50);
     drawSpeed(i);
+    drawOdometer(325743);
+    drawTripMeter(13); // distance in hectometers
+    drawBatteryDist(24);
     display.display();
     delay(100);
   }
-}
-
-
-void loop() {
 }
 
 /* draws the battery indicator on the display
@@ -102,20 +110,54 @@ void drawBattery(uint8_t c){
 /* draws the speed indicator on the display
  *  s is the speed in km/h
  */
-
-
- 
 void drawSpeed(uint8_t s){
   if(s<10){
-    display.drawChar(20,8,'0',WHITE,BLACK,3);
-    display.drawChar(37,8,'0'+s,WHITE,BLACK,3);
+    display.drawChar(SCREEN_SPEED_X,SCREEN_SPEED_Y,'0',WHITE,BLACK,3);
+    display.drawChar(SCREEN_SPEED_X+17,SCREEN_SPEED_Y,'0'+s,WHITE,BLACK,3);
   }
   else{
-    display.drawChar(20,8,'0'+s/10,WHITE,BLACK,3);
-    display.drawChar(37,8,'0'+s%10,WHITE,BLACK,3);
+    display.drawChar(SCREEN_SPEED_X,SCREEN_SPEED_Y,'0'+s/10,WHITE,BLACK,3);
+    display.drawChar(SCREEN_SPEED_X+17,SCREEN_SPEED_Y,'0'+s%10,WHITE,BLACK,3);
   }
-  display.drawChar(25,30,'k',WHITE,BLACK,1);
-  display.drawChar(30,30,'m',WHITE,BLACK,1);
-  display.drawChar(35,30,'/',WHITE,BLACK,1);
-  display.drawChar(40,30,'h',WHITE,BLACK,1);
+  display.drawChar(SCREEN_SPEED_X+6+5*0,SCREEN_SPEED_Y+22,'k',WHITE,BLACK,1);
+  display.drawChar(SCREEN_SPEED_X+6+5*1,SCREEN_SPEED_Y+22,'m',WHITE,BLACK,1);
+  display.drawChar(SCREEN_SPEED_X+6+5*2,SCREEN_SPEED_Y+22,'/',WHITE,BLACK,1);
+  display.drawChar(SCREEN_SPEED_X+6+5*3,SCREEN_SPEED_Y+22,'h',WHITE,BLACK,1);
+}
+
+/* draws the odometer (total distance ever) on the display
+ *  d is the distance in km - we use unsigned long to be able to go > 65535 km
+ */
+void drawOdometer(unsigned long d){
+  display.setCursor(SCREEN_ODO_X,SCREEN_ODO_Y);
+  display.setTextColor(WHITE);
+  display.print("Tot. ");
+  display.print(d);
+  display.print(" km");
+}
+
+/* draws the tripmeter (trip distance) on the display
+ *  d is the distance in hm, hectometers, not a typo 
+ *  we use hm to avoid floating point operations
+ *  8int8_t would limit to 25.5 km trips
+ */
+void drawTripMeter(uint16_t d){
+  display.setCursor(SCREEN_ODO_X,SCREEN_ODO_Y-9);
+  display.setTextColor(WHITE);
+  display.print("Trip ");
+  display.print(d/10);
+  display.print(".");
+  display.print(d%10);
+  display.print(" km");
+}
+
+/* draws the remaining battery (distance) on the display
+ *  d is the distance in km
+ */
+void drawBatteryDist(uint8_t d){
+  display.setCursor(SCREEN_BATTERY_IND_POS_X,SCREEN_BATTERY_IND_POS_Y+SCREEN_BATTERY_IND_HEIGHT+2);
+  display.setTextColor(WHITE);
+  display.setTextWrap(false);
+  display.print(d);
+  display.print("km");
 }
