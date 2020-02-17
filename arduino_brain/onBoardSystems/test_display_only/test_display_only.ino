99a0ebf020161777357930947bb31de82a03a5ad
Quentin Van Overmeere <49268972+vquentin@users.noreply.github.com>
2020-02-17 13:16:50 +0100

    Included speed display

--- arduino_brain/onBoardSystems/test_display_only/test_display_only.ino
+++ arduino_brain/onBoardSystems/test_display_only/test_display_only.ino
@@ -16,7 +16,6 @@ BSD license, check license.txt for more information
 All text above, and the splash screen below must be included in any redistribution
 *********************************************************************/
 
-#include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1305.h>
@@ -24,74 +23,27 @@ All text above, and the splash screen below must be included in any redistributi
 // Used for software SPI
 #define OLED_CLK 13
 #define OLED_MOSI 11
-
 // Used for software or hardware SPI
 #define OLED_CS 10
 #define OLED_DC 8
-
 // Used for I2C or SPI
 #define OLED_RESET 9
 
 // battery-drawing parameters
-#define BATTERY_IND_HEIGHT 40
-#define BATTERY_IND_WIDTH 20
-#define BATTERY_IND_POS_X 106
-#define BATTERY_IND_POS_Y 9
-#define BATTERY_IND_CAP_HEIGHT 5
-#define BATTERY_IND_CAP_WIDTH 10
-#define BATTERY_IND_N 5 // number of rectangles to draw
-#define BATTERY_IND_N_SPACE 2 // margin around the battery capacity indicators
+#define SCREEN_BATTERY_IND_HEIGHT 41
+#define SCREEN_BATTERY_IND_WIDTH 20
+#define SCREEN_BATTERY_IND_POS_X 106
+#define SCREEN_BATTERY_IND_POS_Y 9
+#define SCREEN_BATTERY_IND_CAP_HEIGHT 5
+#define SCREEN_BATTERY_IND_CAP_WIDTH 10
+#define SCREEN_BATTERY_IND_N 5 // number of rectangles to draw
+#define SCREEN_BATTERY_IND_N_SPACE 2 // margin around the battery capacity indicators
+#define SCREEN_BATTERY_IND_N_HEIGHT 6 // height of rectangle for capacity indication. Could be computed but worse for rounding errors
 
 // software SPI
 Adafruit_SSD1305 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 // hardware SPI
 //Adafruit_SSD1305 display(OLED_DC, OLED_RESET, OLED_CS);
-
-// I2C
-//Adafruit_SSD1305 display(OLED_RESET);
-
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
-void fill(unsigned char dat1,unsigned char dat2)
-{
-  unsigned char x,y;
-  for(y=0;y<8;y++)
-  {
-    display.command(0xb0+y);
-    display.command(0x00);
-    display.command(0x10);
-    for(x=0;x<132;x++)
-    {
-      display.data(dat1);
-      display.data(dat2);
-    }
-  }
-}
-
 
 void setup()   {                
   Serial.begin(9600);
@@ -106,287 +58,18 @@ void setup()   {
   display.clearDisplay();   // clears the screen and buffer
   display.display();
 
-
-  
-  // draw a single pixel
-  //display.drawPixel(127, 63, WHITE);
-  //display.display();
-  //delay(1000);
-  //display.clearDisplay();
-  //display.display();
-
-
-
-
-/*
-// Battery indicator
-  display.drawFastVLine( 126, 20, 40, WHITE);
-  display.drawFastVLine( 106, 20, 40, WHITE);
-  display.drawFastHLine(106,60,21,WHITE);
-  display.drawFastVLine(111,15,6,WHITE);
-  display.drawFastVLine(121,15,6,WHITE);
-  display.drawFastHLine(111,15,10,WHITE);
-  display.drawFastHLine(106,20,5,WHITE);
-  display.drawFastHLine(121,20,5,WHITE);
-  */
-  drawBattery(50);
-  display.display();
-  
-/*
-  // draw many lines
-  testdrawline();
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  // draw rectangles
-  testdrawrect();
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  // draw multiple rectangles
-  testfillrect();
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  // draw mulitple circles
-  testdrawcircle();
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  // draw a white circle, 10 pixel radius
-  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  testdrawroundrect();
-  delay(1000);
-  display.clearDisplay();
-
-  testfillroundrect();
-  delay(1000);
-  display.clearDisplay();
-
-  testdrawtriangle();
-  delay(1000);
-  display.clearDisplay();
-   
-  testfilltriangle();
-  delay(1000);
-  display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-  testdrawchar();
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
-  for (uint8_t rot=0; rot < 4; rot++) {
-    display.setRotation(rot);
+// Battery and speed indicator
+  for(uint8_t i = 0;i<=45;i++){
     display.clearDisplay();
-    // text display tests
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0,0);
-    display.println("Hello, world!");
-    display.setTextColor(BLACK, WHITE); // 'inverted' text
-    display.println(3.141592);
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.print("0x"); display.println(0xDEADBEEF, HEX);
+    drawBattery(50);
+    drawSpeed(i);
     display.display();
-    delay(1000);
+    delay(100);
   }
-
-  display.setRotation(0);
-  
-  // miniature bitmap display
-  display.clearDisplay();
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-
-  // invert the display
-  display.invertDisplay(true);
-  delay(1000); 
-  display.invertDisplay(false);
-  delay(1000); 
-
-  // draw a bitmap icon and 'animate' movement
-  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-  */
 }
 
 
 void loop() {
-}
-
-
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
-  randomSeed(666);     // whatever seed
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
-    }
-    display.display();
-    delay(200);
-    
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-	icons[f][XPOS] = random(display.width());
-	icons[f][YPOS] = 0;
-	icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
-}
-
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextWrap(false);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
-}
-
-void testdrawcircle(void) {
-  for (uint8_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-    display.display();
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (uint8_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-    display.display();
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (uint16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    display.display();
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawroundrect(void) {
-  for (uint8_t i=0; i<display.height()/3-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
-    display.display();
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = WHITE;
-  for (uint8_t i=0; i<display.height()/3-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-   
-void testdrawrect(void) {
-  for (uint8_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-    display.display();
-  }
-}
-
-void testdrawline() {  
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display();
-  }
-  for (uint8_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-  }
-  for (int8_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int8_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-  }
-  for (int8_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (uint8_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-  }
-  for (uint8_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
-    display.display();
-  }
-  delay(250);
 }
 
 /* draws the battery indicator on the display
@@ -394,16 +77,45 @@ void testdrawline() {
  */
 void drawBattery(uint8_t c){
   // display the outline of the battery indicator
-  display.drawFastHLine(BATTERY_IND_POS_X,BATTERY_IND_POS_Y+BATTERY_IND_HEIGHT,BATTERY_IND_WIDTH+1,WHITE); // bottom line
-  display.drawFastVLine(BATTERY_IND_POS_X, BATTERY_IND_POS_Y, BATTERY_IND_HEIGHT, WHITE); // first main vertical line
-  display.drawFastVLine(BATTERY_IND_POS_X+BATTERY_IND_WIDTH, BATTERY_IND_POS_Y, BATTERY_IND_HEIGHT, WHITE); // second main vertical line
-  display.drawFastVLine(BATTERY_IND_POS_X+(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2,BATTERY_IND_POS_Y-BATTERY_IND_CAP_HEIGHT,BATTERY_IND_CAP_HEIGHT+1,WHITE); // cap
-  display.drawFastVLine(BATTERY_IND_POS_X+(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2+BATTERY_IND_CAP_WIDTH,BATTERY_IND_POS_Y-BATTERY_IND_CAP_HEIGHT,BATTERY_IND_CAP_HEIGHT+1,WHITE); // cap
-  display.drawFastHLine(BATTERY_IND_POS_X+(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2,BATTERY_IND_POS_Y-BATTERY_IND_CAP_HEIGHT,BATTERY_IND_CAP_WIDTH,WHITE); // cap top
-  display.drawFastHLine(BATTERY_IND_POS_X,BATTERY_IND_POS_Y,(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2,WHITE); // cap- cylinder join
-  display.drawFastHLine(BATTERY_IND_POS_X+BATTERY_IND_CAP_WIDTH+(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2,BATTERY_IND_POS_Y,(BATTERY_IND_WIDTH-BATTERY_IND_CAP_WIDTH)/2,WHITE); // cap-cylinder join 
+  display.drawFastHLine(SCREEN_BATTERY_IND_POS_X,SCREEN_BATTERY_IND_POS_Y+SCREEN_BATTERY_IND_HEIGHT,SCREEN_BATTERY_IND_WIDTH+1,WHITE); // bottom line
+  display.drawFastVLine(SCREEN_BATTERY_IND_POS_X, SCREEN_BATTERY_IND_POS_Y, SCREEN_BATTERY_IND_HEIGHT, WHITE); // first main vertical line
+  display.drawFastVLine(SCREEN_BATTERY_IND_POS_X+SCREEN_BATTERY_IND_WIDTH, SCREEN_BATTERY_IND_POS_Y, SCREEN_BATTERY_IND_HEIGHT, WHITE); // second main vertical line
+  display.drawFastVLine(SCREEN_BATTERY_IND_POS_X+(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2,SCREEN_BATTERY_IND_POS_Y-SCREEN_BATTERY_IND_CAP_HEIGHT,SCREEN_BATTERY_IND_CAP_HEIGHT+1,WHITE); // cap
+  display.drawFastVLine(SCREEN_BATTERY_IND_POS_X+(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2+SCREEN_BATTERY_IND_CAP_WIDTH,SCREEN_BATTERY_IND_POS_Y-SCREEN_BATTERY_IND_CAP_HEIGHT,SCREEN_BATTERY_IND_CAP_HEIGHT+1,WHITE); // cap
+  display.drawFastHLine(SCREEN_BATTERY_IND_POS_X+(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2,SCREEN_BATTERY_IND_POS_Y-SCREEN_BATTERY_IND_CAP_HEIGHT,SCREEN_BATTERY_IND_CAP_WIDTH,WHITE); // cap top
+  display.drawFastHLine(SCREEN_BATTERY_IND_POS_X,SCREEN_BATTERY_IND_POS_Y,(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2,WHITE); // cap- cylinder join
+  display.drawFastHLine(SCREEN_BATTERY_IND_POS_X+SCREEN_BATTERY_IND_CAP_WIDTH+(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2,SCREEN_BATTERY_IND_POS_Y,(SCREEN_BATTERY_IND_WIDTH-SCREEN_BATTERY_IND_CAP_WIDTH)/2,WHITE); // cap-cylinder join 
 
   //display rectangles that indicate charge left
-  //TODO
-  
+  //TODO use drawRect for just the outlines, fillRect for the full symbol
+  for(uint8_t i=0; i< SCREEN_BATTERY_IND_N;i++){
+    if(i*100/SCREEN_BATTERY_IND_N<c){
+      display.fillRect(SCREEN_BATTERY_IND_POS_X+SCREEN_BATTERY_IND_N_SPACE+1,SCREEN_BATTERY_IND_POS_Y+(SCREEN_BATTERY_IND_N-i)*SCREEN_BATTERY_IND_N_SPACE+(SCREEN_BATTERY_IND_N-i-1)*SCREEN_BATTERY_IND_N_HEIGHT,SCREEN_BATTERY_IND_WIDTH-2*SCREEN_BATTERY_IND_N_SPACE-1,SCREEN_BATTERY_IND_N_HEIGHT,WHITE);
+    }
+    else{
+      //comment the line below to avoid including empty rectangles
+      //display.drawRect(SCREEN_BATTERY_IND_POS_X+SCREEN_BATTERY_IND_N_SPACE+1,SCREEN_BATTERY_IND_POS_Y+(SCREEN_BATTERY_IND_N-i)*SCREEN_BATTERY_IND_N_SPACE+(SCREEN_BATTERY_IND_N-i-1)*SCREEN_BATTERY_IND_N_HEIGHT,SCREEN_BATTERY_IND_WIDTH-2*SCREEN_BATTERY_IND_N_SPACE-1,SCREEN_BATTERY_IND_N_HEIGHT,WHITE);
+    }
+  }
+}
+
+/* draws the speed indicator on the display
+ *  s is the speed in km/h
+ */
+
+
+ 
+void drawSpeed(uint8_t s){
+  if(s<10){
+    display.drawChar(20,8,'0',WHITE,BLACK,3);
+    display.drawChar(37,8,'0'+s,WHITE,BLACK,3);
+  }
+  else{
+    display.drawChar(20,8,'0'+s/10,WHITE,BLACK,3);
+    display.drawChar(37,8,'0'+s%10,WHITE,BLACK,3);
+  }
+  display.drawChar(25,30,'k',WHITE,BLACK,1);
+  display.drawChar(30,30,'m',WHITE,BLACK,1);
+  display.drawChar(35,30,'/',WHITE,BLACK,1);
+  display.drawChar(40,30,'h',WHITE,BLACK,1);
 }
